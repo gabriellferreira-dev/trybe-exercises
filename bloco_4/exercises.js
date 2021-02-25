2e30fd95f226ca7bff410eff59aec750ee4a36fc
Gabriel Ferreira <glucas709@gmail.com>
2021-02-25 18:48:42 -0300

    Exercício 4 - Verificando se é maior ou menor que 20

--- bloco_4/exercises.js
+++ bloco_4/exercises.js
@@ -24,3 +24,10 @@ media = sum / numbers.length;
 
 console.log('Média aritimética:', media);
 
+// 4 - Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+if (media > 20) {
+  console.log('Valor maior que 20');
+} else {
+  console.log('Valor menor ou igual a 20');
+}
