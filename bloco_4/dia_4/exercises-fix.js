c93e69f0291d8d47d9f0b2b4c0b8964277de245c
Gabriel Ferreira <glucas709@gmail.com>
2021-03-01 15:38:42 -0300

    Acessando chave e concatenando valores

--- bloco_4/dia_4/exercises-fix.js
+++ bloco_4/dia_4/exercises-fix.js
@@ -8,11 +8,18 @@
 // let medals = { golden: 2, silver: 3 };
 
 let player = {
-  name = 'Marta',
-  lastName = 'Silva',
-  age = 34,
-  medals = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: {
     golden: 2,
     silver: 3
   }
 };
+
+player['fullName'] = player.name + ' ' + player.lastName;
+
+
+// Acesse a chave name , lastName e age . Concatene as informações para fazer um console.log no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
+
+console.log('A jodadora ' + player.fullName + ' tem ' + player.age + ' anos de idade' );
