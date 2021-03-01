e0793fdcd48271bfd769e663481733c33ea18397
Gabriel Ferreira <glucas709@gmail.com>
2021-03-01 15:40:33 -0300

    Inserindo novo objeto a chave

--- bloco_4/dia_4/exercises-fix.js
+++ bloco_4/dia_4/exercises-fix.js
@@ -23,3 +23,7 @@ player['fullName'] = player.name + ' ' + player.lastName;
 // Acesse a chave name , lastName e age . Concatene as informações para fazer um console.log no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
 
 console.log('A jodadora ' + player.fullName + ' tem ' + player.age + ' anos de idade' );
+
+// Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo.
+
+player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
