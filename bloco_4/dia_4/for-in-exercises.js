2e58411902a7e23a3fc306e9579b4873ff1234fa
Gabriel Ferreira <glucas709@gmail.com>
2021-03-01 19:15:21 -0300

    Exercício 4 - For/In retornando valores da chave

--- bloco_4/dia_4/for-in-exercises.js
+++ bloco_4/dia_4/for-in-exercises.js
@@ -5,17 +5,31 @@ let info = {
 };
 
 // 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
-
+console.log('=== 1 ===');
 console.log('Bem-vinda, ' + info.personagem);
+console.log('');
 
 // 2 - Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, imprima o objeto no console.
 
 info['recorrente'] = 'Sim';
 
+console.log('=== 2 ===');
 console.log(info);
+console.log('');
 
 // 3 - Faça um for/in que mostre todas as chaves do objeto.
+console.log('=== 3 ===');
 
 for (let key in info) {
   console.log(key);
 }
+
+console.log('');
+
+// 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto.
+console.log('=== 4 ===');
+
+for (let value in info) {
+  console.log(info[value]);
+}
+console.log('');
