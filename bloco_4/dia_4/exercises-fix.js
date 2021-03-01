f0b49e95542043df5af0c0ff70210a2aa26cc0d9
Gabriel Ferreira <glucas709@gmail.com>
2021-03-01 16:07:15 -0300

    Acessando chave

--- bloco_4/dia_4/exercises-fix.js
+++ bloco_4/dia_4/exercises-fix.js
@@ -31,3 +31,7 @@ player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
 // Acesse a chave bestInTheWorld e faça um console.log no seguinte formato: "A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes".
 
 console.log('A jogadora ' + player.fullName + ' foi eleita a melhor do mundo por ' + Object.keys(player.bestInTheWorld).length + ' vezes');
+
+// Acesse a chave medals e faça um console.log no seguinte formato: "A jogadora possui 2 medalhas de ouro e 3 medalhas de prata".
+
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' de prata');
