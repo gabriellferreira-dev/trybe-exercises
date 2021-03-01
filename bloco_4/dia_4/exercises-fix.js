150d9107dd22cd5a31429438c2bfc4c9d2137011
Gabriel Ferreira <glucas709@gmail.com>
2021-03-01 15:31:49 -0300

    Criando objeto

--- bloco_4/dia_4/exercises-fix.js
+++ bloco_4/dia_4/exercises-fix.js
@@ -1,1 +1,18 @@
 // Exercícios de fixação - OBJETOS
+
+// - Crie um objeto player contendo as variáveis listadas abaixo.
+
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
+
+let player = {
+  name = 'Marta',
+  lastName = 'Silva',
+  age = 34,
+  medals = {
+    golden: 2,
+    silver: 3
+  }
+};
