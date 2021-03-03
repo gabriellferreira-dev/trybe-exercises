d0516655c796bb2bf263577bb7270261cd1167dd
Gabriel Ferreira <glucas709@gmail.com>
2021-03-03 18:32:44 -0300

    Exercício 02 - Retornando índice de maior valor do array

--- bloco_4/dia_4/exercises-functions.js
+++ bloco_4/dia_4/exercises-functions.js
@@ -10,3 +10,21 @@ function isPalindrome(stringValue) {
   }
 }
 console.log(isPalindrome('arara'));
+console.log('');
+
+// 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+function highestValueIndex(arrayNumbers) {
+  let higherNumber = Math.max.apply(null, arrayNumbers);
+  let theBiggest = 0;
+
+  for (let i = 0; i < arrayNumbers.length; i += 1) {
+    if (higherNumber > arrayNumbers[i]) {
+      theBiggest = higherNumber;
+    } else {
+      theBiggest = arrayNumbers[i];
+    }
+  }
+  return theBiggest;
+}
+
+console.log(highestValueIndex([1, 3, -2, 5, 10, 11]));
