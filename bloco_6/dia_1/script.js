61f872db88b8a65b6316f2433c01bd118fe9d7bf
Gabriel Ferreira <glucas709@gmail.com>
2021-03-15 21:33:43 -0300

    Formulário último emprego

--- bloco_6/dia_1/script.js
+++ bloco_6/dia_1/script.js
@@ -1,4 +1,7 @@
 const estado = document.getElementById("estado");
+const checkDate = document.querySelector('.check-date');
+const startDate = document.getElementById('start-date');
+
 const estados = [
   "AC - Acre",
   "AL - Alagoas",
@@ -34,3 +37,28 @@ for (let i = 0; i < estados.length; i += 1) {
   option.innerHTML = estados[i];
   estado.appendChild(option);
 }
+
+function validateDay(date) {
+  if (date[0] < 0 || date[0] > 31) {
+    checkDate.innerHTML = 'Dia inválido';
+  }
+}
+
+function validateMonth(date) {
+  if (date[1] < 0 || date[1] > 12) {
+    checkDate.innerHTML = 'Mês inválido';
+  }
+}
+
+function validateYear(date) {
+  if (date[2] < 0) {
+    checkDate.innerHTML = 'Ano inválido';
+  }
+}
+
+startDate.addEventListener('keyup', () => {
+  let date = startDate.value.split('/');
+  validateDay(date);
+  validateMonth(date);
+  validateYear(date);
+});
