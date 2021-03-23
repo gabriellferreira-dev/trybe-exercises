43b8c59e739d79e1e2ac31ba7a0432eaf42e7312
Gabriel Ferreira <glucas709@gmail.com>
2021-03-23 18:43:33 -0300

    Transformando function nomeada em arrow function

--- BLOCO-7/DIA-1/script.js
+++ BLOCO-7/DIA-1/script.js
@@ -1,13 +1,13 @@
-function testingScope(escopo) {
+const testingScope = (escopo) => {
 	if (escopo === true) {
 		var isEscope = 'Não devo ser utilizada fora do meu escopo (if)';
-		isEscope = ifEscope + ' ótimo, fui utilizada no escopo !';
+		isEscope = `${isEscope}. Ótimo, fui utilizada no escopo !`;
 		console.log(isEscope);
 	} else {
 		var elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
 		console.log(elseScope);
 	}
-	console.log(ifEscope + ' o que estou fazendo aqui ? :O');
-}
+	console.log(`${isEscope}. O que estou fazendo aqui ? :O`);
+};
 
-testingScope(true);
+testingScope(false);
