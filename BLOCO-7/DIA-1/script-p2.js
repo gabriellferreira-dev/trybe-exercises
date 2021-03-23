edc1c92fd27c1395315f4337d70800fbf7e533a0
Gabriel Ferreira <glucas709@gmail.com>
2021-03-23 20:37:00 -0300

    Factor number recursive

--- BLOCO-7/DIA-1/script-p2.js
+++ BLOCO-7/DIA-1/script-p2.js
@@ -1,13 +1,19 @@
 // Exercício 01 sem recursividade
-function factorial(value) {
-	let fact = 1;
-	if (value === 0) {
-		return 1;
-	}
-	for (let i = 2; i <= value; i += 1) {
-		fact *= i;
-	}
-	return fact;
-}
+// function factorial(value) {
+// 	let fact = 1;
+// 	if (value === 0) {
+// 		return 1;
+// 	}
+// 	for (let i = 2; i <= value; i += 1) {
+// 		fact *= i;
+// 	}
+// 	return fact;
+// }
 
-console.log(factorial(1));
+// console.log(factorial(1));
+
+// Exercício 01 recursivo
+
+const factorial = (value) => value === 0 ? 1 : value * factorial(value - 1);
+
+console.log(factorial(5));
