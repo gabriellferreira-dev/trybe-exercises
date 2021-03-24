6a4435ff9124269895bb79ae265f361cc6825ac9
Gabriel Ferreira <glucas709@gmail.com>
2021-03-24 19:16:57 -0300

    Exercício 4 - Função 1 - parte 2

--- BLOCO-7/DIA-1/script-p2.js
+++ BLOCO-7/DIA-1/script-p2.js
@@ -14,33 +14,51 @@
 
 // Exercício 01 recursivo
 
-const factorial = (value) => value === 0 ? 1 : value * factorial(value - 1);
+// const factorial = (value) => value === 0 ? 1 : value * factorial(value - 1);
 
-console.log(factorial(5));
+// console.log(factorial(5));
 
 // Exercício 02
 
-const highestWord = (sValue) => {
-	let words = sValue.split(' ');
-	let	highest = '';
-	let word1 = words[0];
-	for (let i = 1; i < words.length; i += 1) {
-		let word2 = words[i];
-		highest = word1.length > word2.length ? word1 : word2;
-		word1 = highest;
-	}
-	return highest;
-};
+// const highestWord = (sValue) => {
+// 	let words = sValue.split(' ');
+// 	let	highest = '';
+// 	let word1 = words[0];
+// 	for (let i = 1; i < words.length; i += 1) {
+// 		let word2 = words[i];
+// 		highest = word1.length > word2.length ? word1 : word2;
+// 		word1 = highest;
+// 	}
+// 	return highest;
+// };
 
-console.log(highestWord('Maravilhosa graça, seu amor que não falha!'));
+// console.log(highestWord('Maravilhosa graça, seu amor que não falha!'));
 
 // Exercício 03
 
-const numberClicks = document.getElementById('number-clicks');
-const btnClick = document.getElementById('click');
-let i = 0;
+// const numberClicks = document.getElementById('number-clicks');
+// const btnClick = document.getElementById('click');
+// let i = 0;
 
-btnClick.addEventListener('click', () => {
-	i += 1;
-	numberClicks.innerHTML = i;
-});
+// btnClick.addEventListener('click', () => {
+// 	i += 1;
+// 	numberClicks.innerHTML = i;
+// });
+
+// Exercício 04
+
+let skills = ['HTML', 'CSS', 'JAVASCRIPT', 'GIT', 'GITHUB'];
+
+const replaceWith = (phrase, value) => {
+  let string = phrase.split('');
+  let newString = '';
+  
+  for (let i = 0; i < string.length; i += 1) {
+    string[i] = string[i] === 'x' ? value : string[i];
+    newString += string[i];
+  }
+
+  return newString;
+};
+
+console.log(replaceWith('Tryber x aqui', 'Gabriel'));
