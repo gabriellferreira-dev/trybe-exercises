5f9c3f60a2d72c6e06c64e9d49568092bae1c2ae
Gabriel Ferreira <glucas709@gmail.com>
2021-03-24 17:57:32 -0300

    Exercício 2 - parte 2

--- BLOCO-7/DIA-1/script-p2.js
+++ BLOCO-7/DIA-1/script-p2.js
@@ -17,3 +17,19 @@
 const factorial = (value) => value === 0 ? 1 : value * factorial(value - 1);
 
 console.log(factorial(5));
+
+// Exercício 02
+
+const highestWord = (sValue) => {
+	let words = sValue.split(' ');
+	let	highest = '';
+	let word1 = words[0];
+	for (let i = 1; i < words.length; i += 1) {
+		let word2 = words[i];
+		highest = word1.length > word2.length ? word1 : word2;
+		word1 = highest;
+	}
+	return highest;
+};
+
+console.log(highestWord('Maravilhosa graça, seu amor que não falha!'));
