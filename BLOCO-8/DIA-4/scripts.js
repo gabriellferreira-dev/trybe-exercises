bfe5ba1d7c3c8ba1d0150ac8af53db166d0d5635
Gabriel Ferreira <glucas709@gmail.com>
2021-04-06 20:29:34 -0300

    Exercício 02

--- BLOCO-8/DIA-4/scripts.js
+++ BLOCO-8/DIA-4/scripts.js
@@ -1,18 +1,94 @@
 // 1 --------------------------
 
+// const assert = require('assert');
+
+// const arrays = [
+//     ['1', '2', '3'],
+//     [true],
+//     [4, 5, 6],
+// ];
+
+// function flatten() {
+//   let array = arrays.reduce((previous, item) => previous.concat(item), []);
+//   return array;
+// }
+
+// console.log(flatten());
+
+// assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
+
+// 2 ------------------------------------------
+
 const assert = require('assert');
 
-const arrays = [
-    ['1', '2', '3'],
-    [true],
-    [4, 5, 6],
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
 ];
 
-function flatten() {
-  let array = arrays.reduce((previous, item) => previous.concat(item), []);
-  return array;
+function allNames() {
+  const namesConcat = books.reduce((a, {author: {name}}) => a !== '' ? `${a}, ${name}` : name, '');
+  return `Nomes: ${namesConcat}.`;
 }
 
-console.log(flatten());
+console.log(allNames())
 
-assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
+assert.deepStrictEqual(allNames(), 'Nomes: George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.');
