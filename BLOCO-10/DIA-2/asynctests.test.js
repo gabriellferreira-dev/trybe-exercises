fa93f271db44b9349278f40d686af2d6dcc34579
Gabriel Ferreira <glucas709@gmail.com>
2021-04-26 15:39:04 -0300

    Exercise 5

--- BLOCO-10/DIA-2/asynctests.test.js
+++ BLOCO-10/DIA-2/asynctests.test.js
@@ -5,7 +5,7 @@ describe('Exercício 1 - Verificando a chamada da callback', () => {
     const callback = (str) => {
       expect(str).toBe(str.toUpperCase());
     };
-    uppercase('abelha', callback)
+    uppercase('abelha', callback);
     done();
   });
 });
@@ -24,23 +24,47 @@ describe('Exercício 3 - Verificando o resultado da função getUserName com asy
     const id = 4;
     try {
       await getUserName(id);
-    } catch(error) {
+    } catch (error) {
       expect(error).toEqual({ error: 'User with ' + id + ' not found.' });
     }
   });
 });
 
-describe('Exercício 4 - Verificando se lista contém respectivos repositórios', () => {  
+describe('Exercício 4 - Verificando se lista contém respectivos repositórios', () => {
   it('must return true if it finds the repository', async () => {
     try {
-      const listRepository = await getRepos('https://api.github.com/orgs/tryber/repos');
-      const reposToFind = ['sd-01-week4-5-project-todo-list', 'sd-01-week4-5-project-meme-generator'];
+      const listRepository = await getRepos(
+        'https://api.github.com/orgs/tryber/repos'
+      );
+      const reposToFind = [
+        'sd-01-week4-5-project-todo-list',
+        'sd-01-week4-5-project-meme-generator',
+      ];
       reposToFind.forEach((repo) => {
         const repos = listRepository.find((repository) => repo === repository);
         expect(repos).toEqual(repo);
-      })
+      });
     } catch (err) {
       return Promise.reject(err);
     }
   });
-})
+});
+
+describe('Exercício 4 - Descobrindo a saída no console log', () => {
+  beforeEach(() => console.log('1 - beforeEach'));
+  afterEach(() => console.log('1 - afterEach'));
+
+  test('', () => console.log('1 - test'));
+
+  /* 
+    Primeiro irá aparecer 1 - beforeEach, logo após 1 - test e
+    em seguida, 1 - afterEach.
+  */
+  describe('Scoped / Nested block', () => {
+    beforeEach(() => console.log('2 - beforeEach'));
+    afterEach(() => console.log('2 - afterEach'));
+  
+    test('', () => console.log('2 - test'));
+  });
+});
+
